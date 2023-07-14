1e5b2e1ee73eca8202a73c19ea8d4665583acf2c
Ayomide Akintan <emzyakints2005@gmail.com>
2023-07-14 15:44:48 +0100

    update

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>AYOMIDE AKINTAN</h1>
+      <h2>I am a Web Developer</h2>
+      <h2>I am a Graphics Designer</h2>
+      <h2>I am a Freelancer</h2>
+    </>
+  )
+}
+
+export default App
