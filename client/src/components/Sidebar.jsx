8ba9d83791eda998a83ad050c33d4510c15b0b25
Ayomide Akintan <emzyakints2005@gmail.com>
2023-09-27 17:51:04 +0100

    Update

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
+import image from '../assets/Emmanuel-2.jpg'
 
 function Sidebar() {
     const { isLightTheme, light, dark } = useContext(ThemeContext)
     const theme = isLightTheme ? light : dark
     return (
-        <div className='p-10 fixed h-[100vh] lg:ml-10 w-[300px] text-center' style={{ background: theme.bgg, color: theme.syntax }}>
-            Sidebar
+        <div className='Sidebar p-10 fixed h-[100vh] lg:ml-10 w-[300px] text-center' style={{ background: theme.bgg, color: theme.syntax }}>
+            <div className="image w-[200px] h-[200px] m-auto">
+                <img src={image} alt="" />
+            </div>
+            <br />
+            <h1 className='font-bold text-2xl'>Ayomide Akintan</h1>
         </div>
     )
 }
