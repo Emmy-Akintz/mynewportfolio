ad228d5ed6ce4afb1a1771f8ca84988f6071eae1
Ayomide Akintan <emzyakints2005@gmail.com>
2023-09-27 16:28:39 +0100

    Update

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,7 +5,7 @@ function Sidebar() {
     const { isLightTheme, light, dark } = useContext(ThemeContext)
     const theme = isLightTheme ? light : dark
     return (
-        <div className='p-12 fixed h-[100vh] lg:ml-10' style={{ background: theme.bgg, color: theme.syntax }}>
+        <div className='p-10 fixed h-[100vh] lg:ml-10 w-[300px] text-center' style={{ background: theme.bgg, color: theme.syntax }}>
             Sidebar
         </div>
     )
