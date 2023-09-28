9e613e22efef4530a38d3a8e9ca9db0ba9a25ea5
Ayomide Akintan <emzyakints2005@gmail.com>
2023-09-28 07:33:58 +0100

    Update

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ function Sidebar() {
     const { isLightTheme, light, dark } = useContext(ThemeContext)
     const theme = isLightTheme ? light : dark
     return (
-        <div className='Sidebar p-10 fixed h-[100vh] lg:ml-10 w-[300px] text-center overflow-scroll' style={{ background: theme.bgg, color: theme.syntax }}>
+        <div className='Sidebar p-10 fixed h-[100vh] lg:ml-10 w-[300px] text-center overflow-y-scroll' style={{ background: theme.bgg, color: theme.syntax }}>
             <div className="image w-[200px] h-[200px] m-auto">
                 <img src={image} alt="" />
             </div>
