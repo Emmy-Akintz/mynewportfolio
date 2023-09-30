19366e7c5f58661edbfeaa60d0c8cc5eadaad19a
Ayomide Emmanuel Akintan <115672480+Emmy-Akintz@users.noreply.github.com>
2023-09-30 08:26:12 +0100

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { IconContext } from 'react-icons'
 import { BsCodeSlash } from 'react-icons/bs'
 import { ThemeContext } from '../contexts/ThemeContext'
 
@@ -12,7 +13,9 @@ function About() {
         <p className="mt-12 w-[300px] lg:w-[700px]"><span className="font-bold">Hi, I'm Ayomide Emmanuel Akintan (Emmy-Akintz), </span>a tech bro and welcome to my space!<span className='font-bold'>:)</span> I am a software engineer who loves to create websites for people. I think that people should look at the bigger picture when they are building something. I love to work in groups where everyone can voice their opinions and ideas.</p>
         <div className="card p-5 mt-5 w-[300px]" style={{ borderBottom: `3px solid ${theme.ui}`, boxShadow: `0px -20px 50px 20px ${theme.bgg}` }}>
           <div className="">
+<IconContext.Provider value={{ color: theme.ui }}>
             <BsCodeSlash />
+</IconContext.Provider>
           </div>
           <p className="mt-4">
             Web Development
