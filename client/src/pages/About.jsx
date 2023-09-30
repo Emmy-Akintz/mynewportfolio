de7fcb9b894c1d3868a8d7a44c5ea44bd8a44c59
Ayomide Emmanuel Akintan <115672480+Emmy-Akintz@users.noreply.github.com>
2023-09-30 08:12:20 +0100

    Update About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -10,7 +10,7 @@ function About() {
         <h1 className="">ABOUT US</h1>
         <h1 className="font-bold text-2xl">WHO I AM?</h1>
         <p className="mt-12 w-[300px] lg:w-[700px]"><span className="font-bold">Hi, I'm Ayomide Emmanuel Akintan (Emmy-Akintz), </span>a tech bro and welcome to my space!<span className='font-bold'>:)</span> I am a software engineer who loves to create websites for people. I think that people should look at the bigger picture when they are building something. I love to work in groups where everyone can voice their opinions and ideas.</p>
-        <div className="card p-5 mt-5 w-[300px]" style={{ borderBottom: `2px solid ${theme.ui}`, boxShadow: `10px 10px 50px ${theme.bgg}` }}>
+        <div className="card p-5 mt-5 w-[300px]" style={{ borderBottom: `2px solid ${theme.ui}`, boxShadow: `10px 10px 50px 15px ${theme.bgg}` }}>
           <div className="">
             <BsCodeSlash />
           </div>
