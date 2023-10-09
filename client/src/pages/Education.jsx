61b4156265b722a526ff68c8ab926e3713a27ddc
Ayomide Akintan <emzyakints2005@gmail.com>
2023-10-09 15:32:05 +0100

    Update

--- client/src/pages/Education.jsx
+++ client/src/pages/Education.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import { AiOutlinePlus } from 'react-icons/ai'
+import React, { useContext, useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
+import { ThemeContext } from '../contexts/ThemeContext'
 
 function Education() {
+    const { isLightTheme, light, dark } = useContext(ThemeContext)
+    const theme = isLightTheme ? light : dark
+    const [option1, setOption1] = useState(true)
+    const [option2, setOption2] = useState(false)
   return (
     <div className='mt-12'>
         <h1 className="text-sm">EDUCATION</h1>
         <br />
         <h1 className="font-bold">EDUCATION</h1>
         <br />
-        <div className="">
-            <div className="border w-[280px] lg:w-[700px] p-2 flex justify-between items-center">
+        <div onClick={() => setOption1(!option1)} className="block">
+            <div className="w-[280px] lg:w-[700px] p-2 flex justify-between items-center cursor-pointer" style={{ background: theme.ui }}>
                 <p className="text-sm">BACHELOR DEGREE OF COMPUTER SCIENCE</p>
-                <AiOutlinePlus />
+                <AiOutlinePlus className={option1 ? 'hidden' : ''} />
+                <AiOutlineMinus className={option1 ? '' : 'hidden'} />
+            </div>
+            <div className={option1 ? "border p-2 w-[280px] lg:w-[700px] mt-1" : "hidden"}>
+                I am currently studying computer science at Landmark University, Omu Aran, Kwara State, Nigeria. I will get my degree by the year 2025.
+            </div>
+        </div>
+        <div onClick={() => setOption2(!option2)} className="block mt-1">
+            <div className="w-[280px] lg:w-[700px] p-2 flex justify-between items-center cursor-pointer" style={{ background: theme.ui }}>
+                <p className="text-sm">HIGH SCHOOL SECONDARY EDUCATION</p>
+                <AiOutlinePlus className={option2 ? 'hidden' : ''} />
+                <AiOutlineMinus className={option2 ? '' : 'hidden'} />
+            </div>
+            <div className={option2 ? "block border mt-1 p-2 w-[280px] lg:w-[700px]" : "hidden"}>
+                I finished my secondary school education at Nigerian Navy Secondary School, Imeri, Ondo State, Nigeria. My favourite subjects then were mathematics, physics, further mathematics and technical drawing (I wanted to be an engineer 😅).
             </div>
         </div>
     </div>
