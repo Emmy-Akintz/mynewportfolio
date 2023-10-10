a06e28861b8de0b78ae6076328346be981b78b4b
Ayomide Akintan <emzyakints2005@gmail.com>
2023-10-10 17:15:29 +0100

    Update

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -8,12 +8,18 @@ function Contact() {
     const [email, setEmail] = useState('')
     const [subject, setSubject] = useState('')
     const [message, setMessage] = useState('')
+    const [confirm, setConfirm] = useState('')
     const handleSubmit = (e) => {
         e.preventDefault()
         setName('')
         setEmail('')
         setSubject('')
         setMessage('')
+        setConfirm('Delivered!')
+        const timer = setTimeout(() => {
+            setConfirm('')
+        }, 5000)
+        return () => clearTimeout(timer)
     }
     const { isLightTheme, light, dark } = useContext(ThemeContext)
     const theme = isLightTheme ? light : dark
@@ -56,10 +62,11 @@ function Contact() {
                         <input type="email" placeholder='Email' className='my-2 p-2 w-[245px] lg:w-[254px]' style={{ background: theme.bgg }} value={email} onChange={(e) => setEmail(e.target.value)} required />
                         <input type="text" placeholder='Subject' className='my-2 p-2 w-[245px] lg:w-[254px]' style={{ background: theme.bgg }} value={subject} onChange={(e) => setSubject(e.target.value)} required />
                         <textarea cols="30" rows="5" placeholder='Message' className='my-2 p-2' style={{ background: theme.bgg }} value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-                        <div className="flex w-[90%] justify-between">
-                            <button type="submit" className='py-2 px-1 rounded  ' style={{ background: theme.ui }}>
+                        <div className="flex w-[80%] justify-between">
+                            <button type="submit" className='py-2 px-1 rounded' style={{ background: theme.ui }}>
                                 SEND MESSAGE
                             </button>
+                            <div className="p-2 rounded" style={confirm ? {border: `1px solid ${theme.ui}`} : {border: 'none'}} onChange={(e) => setConfirm(e.target.value)}>{confirm}</div>
                             {/* <button type="reset" onClick={console.log('hello')} className='py-2 px-1 rounded' style={{ background: theme.ui }}>
                                 CLEAR MESSAGE
                             </button> */}
