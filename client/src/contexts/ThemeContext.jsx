e8fe4edd31914f2d949bbfc49d05264fad5bbebb
Ayomide Akintan <emzyakints2005@gmail.com>
2023-10-10 17:32:31 +0100

    Update

--- client/src/contexts/ThemeContext.jsx
+++ client/src/contexts/ThemeContext.jsx
@@ -5,22 +5,22 @@ export const ThemeContext = createContext()
 const ThemeContextProvider = (props) => {
     const [lightTheme, setLightTheme] = useState({
         isLightTheme: true,
-        light: { syntax: '#333333', ui: '#2196f3', bg: '#ffffff', bgg: '#f5f5f5'},
-        dark: { syntax: '#d3d3d3', ui: '#ff5722', bg: '#121212', bgg: '#1e1e1e'}
+        light: { syntax: '#333333', ui: '#2196f3', bg: '#ffffff', bgg: '#f5f5f5' },
+        dark: { syntax: '#d3d3d3', ui: '#ff5722', bg: '#121212', bgg: '#1e1e1e' }
     })
     const toggleTheme = (isLightTheme) => {
         setLightTheme(previousTheme => {
             return {
-                ...previousTheme, 
+                ...previousTheme,
                 isLightTheme: isLightTheme
             }
         })
     }
-    return ( 
+    return (
         <ThemeContext.Provider value={{ ...lightTheme, toggleTheme }}>
-            { props.children }
+            {props.children}
         </ThemeContext.Provider>
-     );
+    );
 }
- 
+
 export default ThemeContextProvider;
