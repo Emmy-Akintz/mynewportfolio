eba4f0755695b6a17b7d0a95eeaca609ac50c488
Ayomide Akintan <emzyakints2005@gmail.com>
2023-11-03 10:08:27 +0100

    Update

--- client/src/pages/Work.jsx
+++ client/src/pages/Work.jsx
@@ -19,7 +19,7 @@ function Work() {
             <div className="md:grid lg:grid md:grid-cols-2 lg:grid-cols-2 w-[280px] md:w-[650px] lg:w-[650px]">
                 <SlideInLeft>
                     <div className="card w-[280px] my-4 p-4 rounded" style={{ background: theme.ui }}>
-                        <img src="https://random.imagecdn.app/v1/image?width=500&height=150" alt="" className='mb-4 rounded' />
+                        <iframe src='https://larry-pat-foods.netlify.app/' title='Larry-Pat Page' width='100%' className='mb-4'></iframe>
                         <p className="font-bold">WORK 01</p>
                         <p className="text-sm">LARRY-PAT FOODS</p>
                         <br />
@@ -38,7 +38,7 @@ function Work() {
                 </SlideInLeft>
                 <SlideInRight>
                     <div className="card w-[280px] my-4 p-4 rounded" style={{ background: theme.ui }}>
-                        <img src="https://random.imagecdn.app/v1/image?width=500&height=150" alt="" className='mb-4 rounded' />
+                        <iframe src='https://pre-hackathon.netlify.app/' title='Hackathon Page' width='100%' className='mb-4'></iframe>
                         <p className="font-bold">WORK 02</p>
                         <p className="text-sm">PRE-HACKATHON</p>
                         <br />
