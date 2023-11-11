b1db5d9c3f5d51a75822a50590ee3924c7d37c76
Ayomide Akintan <emzyakints2005@gmail.com>
2023-11-11 08:01:53 +0100

    pdate

--- client/src/contexts/ThemeContext.jsx
+++ client/src/contexts/ThemeContext.jsx
@@ -5,7 +5,7 @@ export const ThemeContext = createContext()
 const ThemeContextProvider = (props) => {
     const [lightTheme, setLightTheme] = useState({
         isLightTheme: false,
-        light: { syntax: '#333333', ui: '#2196f3', bg: '#ffffff', bgg: '#f5f5f5' },
+        light: { syntax: '#333333', ui: '#30d5c8', bg: '#ffffff', bgg: '#f5f5f5' },
         dark: { syntax: '#d3d3d3', ui: '#50c878', bg: '#121212', bgg: '#1e1e1e' }
     })
     const toggleTheme = (isLightTheme) => {
