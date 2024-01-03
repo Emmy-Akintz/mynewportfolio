4af973ae282babbd69dc8732ad73f6db9ec89b85
Ayomide Akintan <emzyakints2005@gmail.com>
2024-01-03 12:40:13 +0100

    implemented automatic detection of user's system theme

--- client/src/contexts/ThemeContext.jsx
+++ client/src/contexts/ThemeContext.jsx
@@ -1,13 +1,23 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 export const ThemeContext = createContext()
 
 const ThemeContextProvider = (props) => {
     const [lightTheme, setLightTheme] = useState({
-        isLightTheme: false,
+        isLightTheme: true,
         light: { syntax: '#333333', ui: '#30d5c8', bg: '#ffffff', bgg: '#f5f5f5' },
         dark: { syntax: '#d3d3d3', ui: '#50c878', bg: '#121212', bgg: '#1e1e1e' }
     })
+
+    // detect default theme
+    useEffect(() => {
+        const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+        setLightTheme(previousTheme => ({
+            ...previousTheme,
+            isLightTheme: !isDark // change to dark if that's the user's preference
+        }))
+    }, [])
+
     const toggleTheme = (isLightTheme) => {
         setLightTheme(previousTheme => {
             return {
@@ -16,6 +26,7 @@ const ThemeContextProvider = (props) => {
             }
         })
     }
+    
     return (
         <ThemeContext.Provider value={{ ...lightTheme, toggleTheme }}>
             {props.children}
