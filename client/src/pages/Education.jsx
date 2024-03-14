bc283fe01e35e782bc5c344994b18d142db7bb57
Ayomide Akintan <emzyakints2005@gmail.com>
2024-03-14 14:40:44 +0100

    worked on education.jsx

--- client/src/pages/Education.jsx
+++ client/src/pages/Education.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 import { ThemeContext } from '../contexts/ThemeContext'
 import { FadeUp, SlideInLeft, SlideInRight } from '../components/Reveal'
@@ -6,10 +6,50 @@ import { FadeUp, SlideInLeft, SlideInRight } from '../components/Reveal'
 function Education() {
     const { isLightTheme, light, dark } = useContext(ThemeContext)
     const theme = isLightTheme ? light : dark
-    const [option1, setOption1] = useState(true)
-    const [option2, setOption2] = useState(false)
-    const [option3, setOption3] = useState(false)
-    const [option4, setOption4] = useState(false)
+    const [option1, setOption1] = useState()
+    const [option2, setOption2] = useState()
+    const [option3, setOption3] = useState()
+    const [option4, setOption4] = useState()
+
+    const [option, setOption] = useState(1)
+
+    useEffect(() => {
+        setOption(1);
+        setOption1(true);
+        setOption2(false);
+        setOption3(false);
+        setOption4(false);
+    }, []);
+
+    switch (option) {
+        case 1:
+            setOption(true)
+            setOption2(false)
+            setOption3(false)
+            setOption4(false)
+            break;
+        case 2:
+            setOption(false)
+            setOption2(true)
+            setOption3(false)
+            setOption4(false)
+            break
+        case 3:
+            setOption(false)
+            setOption2(false)
+            setOption3(true)
+            setOption4(false)
+            break
+        case 4:
+            setOption(false)
+            setOption2(false)
+            setOption3(false)
+            setOption4(true)
+            break
+        default:
+            break;
+    }
+
     return (
         <div id='education' className='mt-12'>
             <FadeUp>
@@ -21,7 +61,16 @@ function Education() {
             </FadeUp>
             <br />
             <SlideInLeft>
-                <div onClick={() => setOption1(!option1)} className="block">
+                <div onClick={() => {
+                    setOption1(!option1)
+                    if (option1 === false) {
+                        setOption(1)
+                    } else {
+                        setOption2(false)
+                        setOption3(false)
+                        setOption4(false)
+                    }
+                }} className="block">
                     <div className="w-[280px] md:w-[650px] lg:w-[610px] p-2 rounded flex justify-between items-center cursor-pointer" style={{ background: theme.ui }}>
                         <p className="text-sm">BACHELOR DEGREE OF COMPUTER SCIENCE</p>
                         <AiOutlinePlus className={option1 ? 'hidden' : ''} />
@@ -33,7 +82,13 @@ function Education() {
                 </div>
             </SlideInLeft>
             <SlideInRight>
-                <div onClick={() => setOption2(!option2)} className="block mt-1">
+                <div onClick={() => {
+                    setOption2(!option2)
+                    if (option2 === false) {
+                        setOption(2)
+                        setOption1(false)
+                    }
+                }} className="block mt-1">
                     <div className="w-[280px] w-[280px] md:w-[650px] lg:w-[610px] p-2 rounded flex justify-between items-center cursor-pointer" style={{ background: theme.ui }}>
                         <p className="text-sm">HIGH SCHOOL SECONDARY EDUCATION</p>
                         <AiOutlinePlus className={option2 ? 'hidden' : ''} />
@@ -45,7 +100,13 @@ function Education() {
                 </div>
             </SlideInRight>
             <SlideInLeft>
-                <div onClick={() => setOption3(!option3)} className="block mt-1">
+                <div onClick={() => {
+                    setOption3(!option3)
+                    if (option3 === false) {
+                        setOption(3)
+                        setOption1(false)
+                    }
+                }} className="block mt-1">
                     <div className="w-[280px] md:w-[650px] lg:w-[610px] p-2 rounded flex justify-between items-center cursor-pointer" style={{ background: theme.ui }}>
                         <p className="text-sm">WEB DEVELOPMENT TRAINING</p>
                         <AiOutlinePlus className={option3 ? 'hidden' : ''} />
@@ -59,7 +120,13 @@ function Education() {
                 </div>
             </SlideInLeft>
             <SlideInRight>
-                <div onClick={() => setOption4(!option4)} className="block mt-1">
+                <div onClick={() => {
+                    setOption4(!option4)
+                    if (option4 === false) {
+                        setOption(4)
+                        setOption1(false)
+                    }
+                }} className="block mt-1">
                     <div className="w-[280px] md:w-[650px] lg:w-[610px] p-2 rounded flex justify-between items-center cursor-pointer" style={{ background: theme.ui }}>
                         <p className="text-sm">PRIMARY SCHOOL EDUCATION</p>
                         <AiOutlinePlus className={option4 ? 'hidden' : ''} />
